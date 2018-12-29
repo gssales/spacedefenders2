0c4aaf592ff8f660081b405adf1d64d18fffde41
Guilherme Sales <gssales@inf.ufrgs.br>
2018-12-29 15:24:02 -0200

    Fix Player movement and add Shield joint

--- objects/gameobjects/Player.lua
+++ objects/gameobjects/Player.lua
@@ -5,13 +5,13 @@ function Player:new (area, x, y, opts)
 
 	self.depth = 50
 
+	self.m = 100
 	self.w = 64
 	self.collider = self.area.world:newCircleCollider(self.x, self.y, self.w)
 	self.collider:setCollisionClass('Player')
-	self.collider:setPosition(self.x, self.y)
 	self.collider:setObject(self)
-	self.collider:setFixedRotation(false)
-	self.m = 100
+	self.collider:setRestitution(0.7)
+	self.collider:setFriction(0.8)
 	self.collider:setMass(self.m)
 
 	self.r = 0
@@ -20,16 +20,16 @@ function Player:new (area, x, y, opts)
 	self.max_v = 600
 	self.a = 100
 
+	self.can_shoot = true
+	self.move = false
+
+	self.sprite = love.graphics.newImage('resources/sprites/player_ship.png')
+
 	self.propeler_r_min = 5
 	self.propeler_r_max = 15
 	self.propeler_c1 = colors.white
 	self.propeler_c2 = colors.propeler_yellow
 	self.propeler_c3 = colors.propeler_red
-
-	self.can_shoot = true
-
-	self.sprite = love.graphics.newImage('resources/sprites/player_ship.png')
-
 	self.timer:every(0.02, function ()
 		local x1 = 36*math.cos(self.r-math.pi/2)-48*math.cos(self.r)
 		local y1 = 36*math.sin(self.r-math.pi/2)-48*math.sin(self.r)
@@ -43,7 +43,14 @@ function Player:new (area, x, y, opts)
 			d = randomFloat(0.15, 0.25), c1 = self.propeler_c1, c2 = self.propeler_c2, c3 = self.propeler_c3})
 	end)
 
-	self.area:addGameObject('Shield', self.x, self.y, {w = 1.5*self.w, player = self})
+	input:bind('x', function ()
+		if self.shield_obj then
+			self:unshield()
+		else
+			self:shield()
+		end
+		print(self.joint)
+	end)
 
 end
 
@@ -59,17 +66,26 @@ function Player:update (dt)
 	end
 
 	if input:down('left') then
+		self.move = true
+		local vx, vy = self.collider:getLinearVelocity()
+		self.v = math.sqrt(vx*vx + vy*vy)
 		self.r = self.r - self.rv*dt
 	end
 	if input:down('right') then
+		self.move = true
+		local vx, vy = self.collider:getLinearVelocity()
+		self.v = math.sqrt(vx*vx + vy*vy)
 		self.r = self.r + self.rv*dt
 	end
 	if input:down('up') and not self.v_timer then
+		self.move = true
 		self.v = math.min(self.v + self.a*dt, self.max_v)
 	end
 	if input:down('down') and not self.v_timer then
+		self.move = true
 		self.v = math.max(self.v - self.a*dt, 100)
 	end
+
 	self.propeler_r_min = 10
 	self.propeler_r_max = 15
 	self.propeler_c1 = colors.white
@@ -77,12 +93,14 @@ function Player:update (dt)
 	self.propeler_c3 = colors.propeler_red
 
 	if input:down('boost') then
+		self.move = true
 		self.v = math.min(self.v + self.a*dt, 2*self.max_v)
 		self.propeler_c1 = colors.propeler_blue
 		self.propeler_c2 = colors.white
 		self.propeler_c3 = colors.propeler_blue
 	end
-	if input:released('boost') then
+	if input:released('boost') and self.v > self.max_v then
+		self.move = true
 		self.v_timer = self.timer:tween(0.5, self, {v = self.max_v}, 'linear', function ()
 			self.v_timer = nil
 		end)
@@ -93,13 +111,15 @@ function Player:update (dt)
 		self.propeler_r_max = 10
 	end
 
-	self.collider:setLinearVelocity(self.v * math.cos(self.r-math.pi/2), self.v * math.sin(self.r-math.pi/2))
+	if self.move then
+		self.move = false
+		self.collider:setLinearVelocity(self.v * math.cos(self.r-math.pi/2), self.v * math.sin(self.r-math.pi/2))
+	end
 end
 
 function Player:draw ()
 	love.graphics.setColor(colors.white)
 	love.graphics.draw(self.sprite, self.x, self.y, self.r, 1, 1, self.sprite:getWidth()/2, self.sprite:getHeight()/2)
-	--love.graphics.circle('line', self.x, self.y, self.w)
 end
 
 function Player:shoot ()
@@ -111,4 +131,16 @@ function Player:shoot ()
 	self.area:addGameObject('Shot',
 		self.x + d * math.cos(self.r-math.pi/2), self.y + d * math.sin(self.r-math.pi/2),
 		{r = self.r, pv = self.v})
+end
+
+function Player:shield ()
+	self.shield_obj = self.area:addGameObject('Shield', self.x, self.y, {w = 1.5*self.w})
+	self.joint = self.area.world:addJoint('RevoluteJoint', self.collider, self.shield_obj.collider, self.x, self.y, false)
+end
+function Player:unshield ()
+	-- self.joint:destroy()
+	self.area.world:removeJoint(self.joint)
+	self.shield_obj:die()
+	self.joint = nil
+	self.shield_obj = nil
 end
--- objects/gameobjects/Shield.lua
+++ objects/gameobjects/Shield.lua
@@ -7,6 +7,7 @@ function Shield:new (area, x, y, opts)
 	self.w = opts.w or 80
 	self.collider = self.area.world:newCircleCollider(self.x, self.y, self.w)
 	self.collider:setCollisionClass('Shield')
+	self.collider:setMass(200)
 	self.collider:setObject(self)
 
 	self.sprite = love.graphics.newImage('/resources/sprites/shield.png')
@@ -15,14 +16,13 @@ end
 
 function Shield:update (dt)
 	Shield.super.update(self, dt)
-
-	if self.player then
-		self.x = self.player.x
-		self.y = self.player.y
-	end
 end
 
 function Shield:draw ()
 	love.graphics.setColor(colors.propeler_blue)
 	love.graphics.draw(self.sprite, self.x, self.y, 0, 0.8, 0.8, self.sprite:getWidth()/2, self.sprite:getHeight()/2)
+end
+
+function Shield:die ()
+	self.dead = true
 end
